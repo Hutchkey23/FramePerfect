1cbc768666e856f336cfc2d469c55152c3c63c44
Nathaniel Lackey <hutchkey@gmail.com>
2026-05-10 09:31:08 -0500

    Added keep moving level

--- scripts/utility/level_controller.gd
+++ scripts/utility/level_controller.gd
@@ -52,9 +52,9 @@ const ANALOG_START_THRESHOLD: float = 0.55
 var analog_start_was_pressed: bool = false
 
 func _ready() -> void:
-	 ##DEBUG
-	#enter_intro_state()
-	 ##END DEBUG
+	 #DEBUG
+	enter_intro_state()
+	 #END DEBUG
 	
 	connect_signals()
 	player.position = player_spawn.position
